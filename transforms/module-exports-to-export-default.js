7920eafaae05124dab5a11c2c9132daba6b06169
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2017-07-23 18:29:40 -0400

    Add new transform module.exports to export default

---
+++ transforms/module-exports-to-export-default.js
@@ -0,0 +1,48 @@
+/**
+ * Transform
+ *
+ *   module.exports = *;
+ *
+ * to
+ *
+ *   export default *;
+ *
+ * Only on global context
+ */
+
+import Logger from './utils/logger'
+import {isTopNode} from './utils/filters'
+
+
+function transformer(file, api, options) {
+  const j = api.jscodeshift
+  const logger = new Logger(file, options)
+
+  // ------------------------------------------------------------------ SEARCH
+  const nodes = j(file.source)
+    .find(j.ExpressionStatement, {
+      expression: {
+        left: {
+          object: {
+            name: 'module'
+          },
+          property: {
+            name: 'exports'
+          }
+        },
+        operator: '='
+      }
+    }).filter(isTopNode)
+
+  logger.log(`${nodes.length} nodes will be transformed`)
+
+  if (nodes.length > 1)
+    logger.warn('There should not be more than one `module.exports` declaration in a file')
+
+  // ----------------------------------------------------------------- REPLACE
+  return nodes.replaceWith(path => {
+    return j.exportDefaultDeclaration(path.node.expression.right)
+  }).toSource()
+}
+
+export default transformer
