2ba3529e424913be3ffd3dd096cad8a23ab84aa7
azu <azuciao@gmail.com>
2020-08-22 22:27:05 +0900

    chore: add convert

--- bin/cmd.js
+++ bin/cmd.js
@@ -2,7 +2,7 @@
 "use strict";
 const path = require("path");
 require("codemod-cli").runTransform(
-    path.join(__dirname, "../transforms/index.js"),
+    path.join(__dirname, "../lib/index.js"),
     process.argv[2],
     process.argv.slice(3),
     "jsx,tsx"
