90ed6902e7003c1a2d76b104d30bd86e5130a520
Ville Saukkonen <villesau@msn.com>
2021-05-28 23:51:53 +0300

    test variables

--- transforms/__testfixtures__/module-exports-to-named-export/basic-case-exports.input.js
+++ transforms/__testfixtures__/module-exports-to-named-export/basic-case-exports.input.js
@@ -4,3 +4,9 @@ exports.a = function () {
 exports.b = function () {
   return "b";
 };
+
+const c = () => {
+  return 42;
+};
+exports.c = c;
+exports.d = c;
--- transforms/__testfixtures__/module-exports-to-named-export/basic-case-exports.output.js
+++ transforms/__testfixtures__/module-exports-to-named-export/basic-case-exports.output.js
@@ -5,3 +5,9 @@ export const a = function () {
 export const b = function () {
   return "b";
 };
+
+const c = () => {
+  return 42;
+};
+export { c };
+export { c as d };
